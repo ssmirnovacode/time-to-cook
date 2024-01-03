e4c197e41e55cddcd236643fdcf8d78e6062e588
ssmirnovacode <svetlana.smirnova213@gmail.com>
2024-01-03 14:51:26 +0100

    MealItem test added

--- app/components/tests/MealItem.test.tsx
+++ app/components/tests/MealItem.test.tsx
@@ -13,7 +13,15 @@ const PROPS = {
 describe("<MealItem />", () => {
   it("renders with required props", () => {
     render(<MealItem {...PROPS} />);
-    const h2 = screen.getByRole("heading", { level: 2 });
-    expect(h2).toHaveTextContent(PROPS.title);
+    expect(screen.getByRole("heading", { level: 2 })).toHaveTextContent(
+      PROPS.title
+    );
+    expect(screen.getByAltText(PROPS.title)).toBeInTheDocument();
+    expect(screen.getByText(`by ${PROPS.creator}`)).toBeInTheDocument();
+    expect(screen.getByText(PROPS.summary)).toBeInTheDocument();
+    expect(screen.getByRole("link")).toHaveAttribute(
+      "href",
+      `/meals/${PROPS.slug}`
+    );
   });
 });
