3db42184c7ed60f60ea23563e67ee623b0ca17cd
ssmirnovacode <svetlana.smirnova213@gmail.com>
2024-01-04 19:17:55 +0100

    wip: bug in server actions must be async

--- app/meals/share/page.tsx
+++ app/meals/share/page.tsx
@@ -1,32 +1,15 @@
+"use client";
 import ImagePicker from "@/app/components/ImagePicker/ImagePicker";
-import styles from "./page.module.css";
 import { saveMeal } from "@/app/services/meals";
+import styles from "./page.module.css";
 import { redirect } from "next/navigation";
 import MealFormSubmit from "@/app/components/Meals/MealFormSubmit";
+import { useFormState } from "react-dom";
+import { shareMeal } from "@/app/services/actions";
 
 export default function ShareMealPage() {
-  // can be moved to separate file to be used in a client component
-  async function shareMeal(formData: FormData) {
-    "use server";
-
-    const meal = {
-      title: formData.get("title") as string,
-      summary: formData.get("summary") as string,
-      instructions: formData.get("instructions") as string,
-      image: formData.get("image") as File,
-      creator: formData.get("name") as string,
-      creator_email: formData.get("email") as string,
-    };
-    //console.log("meal", meal);
-
-    const result = await saveMeal(meal);
-
-    if (result) {
-      redirect("/meals");
-    } else {
-      // TODO show error
-    }
-  }
+  // TODO resolve TS error with shareMEal
+  const [state, formAction] = useFormState(shareMeal as any, { message: null });
 
   return (
     <>
@@ -37,7 +20,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={styles.main}>
-        <form className={styles.form} action={shareMeal}>
+        <form className={styles.form} action={formAction}>
           <div className={styles.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -66,6 +49,7 @@ export default function ShareMealPage() {
             ></textarea>
           </p>
           <ImagePicker label={"Your image"} name="image" />
+          {state.message && <p style={{ color: "red" }}>{state.message}</p>}
           <p className={styles.actions}>
             <MealFormSubmit />
           </p>
