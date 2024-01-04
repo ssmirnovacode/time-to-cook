668265e047b1c21d71fe537ac9869dcb7b9b1758
ssmirnovacode <svetlana.smirnova213@gmail.com>
2024-01-04 12:19:31 +0100

    not-found route added

---
+++ app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function PageNotFound() {
+  return (
+    <main className="not-found">
+      <h1>Not found</h1>
+      <p>Unfortunately the requested page does not seem to exist...</p>
+    </main>
+  );
+}
