7c96c97ea931a62b97f61b71fa72bdc2f7ac7c6b
ssmirnovacode <svetlana.smirnova213@gmail.com>
2024-01-04 12:49:18 +0100

    not-found added for MealDetails

---
+++ app/meals/[slug]/not-found.tsx
@@ -0,0 +1,8 @@
+export default function MealNotFound() {
+  return (
+    <main className="not-found">
+      <h1>Meal not found</h1>
+      <p>Unfortunately the requested meal does not seem to exist...</p>
+    </main>
+  );
+}
--- app/meals/[slug]/page.tsx
+++ app/meals/[slug]/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import { getMeal } from "@/app/services/meals";
 import { Meal } from "@/app/types";
+import { notFound } from "next/navigation";
 
 export default async function MealDetailsPage({
   params: { slug = "" },
@@ -9,9 +10,15 @@ export default async function MealDetailsPage({
   params: { slug: string };
 }) {
   const meal: Meal = await getMeal(slug);
+
+  if (!meal) {
+    notFound();
+  }
+
   let { title, image, summary, creator, creator_email, instructions } =
     meal || {};
   instructions = instructions.replace(/\n/g, "<br />");
+
   return (
     <>
       <header className={styles.header}>
